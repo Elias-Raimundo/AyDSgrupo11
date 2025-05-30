81aeac6349b267b6538726d3f9c8cabec367eeea
Elias <eliasraimundo2005@gmail.com>
2025-05-30 20:43:56 -0300

    database connection and models with server

--- models/saving.rb
+++ models/saving.rb
@@ -1,28 +1,30 @@
 class Saving < ActiveRecord::Base
-    belongs_to :account
-  
-    validates :account, presence: true
-    validates :amount, numericality: { greater_than: 0 }
-    validates :name, presence: true
-  
-    after_create :deduct_from_balance
-    after_destroy :restore_to_balance
-  
-    private
-  
-    def deduct_from_balance
-      ActiveRecord::Base.transaction do
-        raise "Insufficient funds" if account.balance < amount
-  
-        account.balance -= amount
-        account.save!
-      end
+  belongs_to :account
+
+  validates :account, presence: true
+  validates :amount, numericality: { greater_than: 0 }
+  validates :name, presence: true
+
+  after_create :deduct_from_balance
+  after_destroy :restore_to_balance
+
+  private
+
+  def deduct_from_balance
+    ActiveRecord::Base.transaction do
+      raise "Insufficient funds" if account.balance < amount
+
+      account.balance -= amount
+      account.save!
     end
-  
-    def restore_to_balance
-      ActiveRecord::Base.transaction do
-        account.balance += amount
-        account.save!
-      end
-    end
-  end
+  end
+
+  def restore_to_balance
+    ActiveRecord::Base.transaction do
+      account.balance += amount
+      account.save!
+    end
+  end
+end
+
+
--- server.rb
+++ server.rb
@@ -1,6 +1,18 @@
 require 'bundler/setup'
 require 'sinatra/base'
 require 'sinatra/reloader' if Sinatra::Base.environment == :development
+require 'yaml'
+require 'active_record'
+require 'erb'
+require 'logger'
+require 'bigdecimal'
+#require_relative 'file'
+
+
+db_config = YAML.load_file('config/database.yml', aliases: true)
+ActiveRecord::Base.establish_connection(db_config['development'])
+
+Dir[File.join(__dir__, 'models', '*.rb')].each { |file| require_relative file }
 
 class App < Sinatra::Application
   configure :development do
@@ -11,7 +23,55 @@ class App < Sinatra::Application
   end
 
   set :views, File.expand_path('../views', __FILE__)
-  
+   # Activa sesiones (para mantener al usuario logueado)
+  enable :sessions
+
+  # Ruta raíz: redirige al login
+#  get '/' do
+#    redirect '/login'
+#  end
+
+  # Ruta para mostrar el formulario de registro
+  get '/signup' do
+    erb :signup
+  end  
+
+  # Procesa el formulario de registro
+  post '/signup' do
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password] # bcrypt lo convierte en password_digest
+    )
+
+    if user.save
+      session[:user_id] = user.id
+      redirect '/welcome'
+    else
+      @error = user.errors.full_messages.join(', ')
+      erb :signup
+    end
+  end
+
+  # Muestra el formulario de login
+  get '/login' do
+    erb :login
+  end
+  # Procesa el login
+  post '/login' do
+    user = User.find_by(email: params[:email])
+
+    # Verifica que el usuario exista y la contraseña sea válida
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/welcome'
+    else
+      @error = 'Email o contraseña incorrectos'
+      erb :login
+    end
+  end
+
+
   get '/' do
     erb :welcome
   end
